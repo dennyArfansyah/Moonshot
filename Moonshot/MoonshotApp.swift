814243e7f626232c5fcc2001334e2b0acc4cb93a
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-18 16:57:20 +0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Denny Arfansyah on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
